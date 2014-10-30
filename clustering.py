277ed92f1bc054b0b97b5c7060f2bcdc43eb6582
Ritz <stammer.3@osu.edu>
2014-10-30 19:52:50 -0400

    added kmeans notes

--- clustering.py
+++ clustering.py
@@ -1,9 +1,16 @@
 import Orange
 import random
+import orngClustering
+import pylab
+import random
 import numpy as np
 from scipy import cluster
 from matplotlib import pyplot
+from scipy.cluster.vq import vq, kmeans, whiten
 
+import math
+import sys
+from Orange import statc
 #from matplotlib import pyplot as plt
 
 def main():
@@ -12,8 +19,16 @@ def main():
   random.seed(42)
   data_input = Orange.data.Table("out.arff")  
 
+
+
+  #MxN array is an observable vector. columns are the features seen during each observation
+  #scipy.cluster.vq.kmeans(obs, k_or_guess, iter=20, thresh=1.0000000000000001e-05)
+  #class Orange.clustering.kmeans.Clustering(data=None, centroids=3, maxiters=None, minscorechange=None, stopchanges=0, nstart=1, initialization=init_random, distance=Orange.distance.Euclidean, scoring=score_distance_to_centroids, inner_callback=None, outer_callback=None)
+
   # Running kmeans algorithm here, need to determine what should go in # field
-  km = Orange.clustering.kmeans.Clustering(data_input, 10)
+  km = Orange.clustering.kmeans.Clustering(data_input, 5, 0 , callback)
+  print km.clusters[-1000:]
+  #not sure why -10, callback not working?
 
   # Trying to figure out how to make a pretty plot but there's some library issues
   #Orange.clustering.kmeans.plot_silhouette(km, "kmeans-silhouette.svg")
@@ -47,4 +62,9 @@ def main():
   Orange.clustering.hierarchical.dendrogram_draw("hclust-dendrogram.svg", root, labels=labels)
 
 
+def callback(km):
+    print "Iteration: %d, changes: %d, score: %.4f" % (km.iteration, km.nchanges, km.score)
+
 main()
+
+
