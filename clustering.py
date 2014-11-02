27c0ac22bded66a8c602d1776062f88af6fdbf7f
Annelise Wittenberg <wittenberg.18@osu.edu>
2014-11-02 13:13:50 -0500

    leedle leedle leedle lee

--- clustering.py
+++ clustering.py
@@ -19,13 +19,10 @@ def main():
   random.seed(42)
   data_input = Orange.data.Table("out.arff")
 
-  # Running kmeans algorithm here, need to determine what should go in # field
-  km = Orange.clustering.kmeans.Clustering(data_input, 3)
-  print km.clusters[-1000:]
-
   # Calculate Euclidean Distance
   measure = Orange.distance.Euclidean(data_input)
   print measure(data_input[0], data_input[1])
+
 
   # Calculate Manhattan Distance
   poop = Orange.distance.Manhattan(data_input)
@@ -35,20 +32,29 @@ def main():
   matrix = Orange.distance.distance_matrix(data_input)
   #print matrix[0,1]
 
-
+   # Running kmeans algorithm here, need to determine what should go in # field
+  km = Orange.clustering.kmeans.Clustering(data_input, 10)
+  print km.clusters[-1000:]
 
   # Data Generation (rand(150,2) + array([.5,.5]),rand(150,2))
   dat = vstack(matrix)
 
   # Computing k means with K = 3
-  centroids,_ = kmeans(dat, 3)
+  centroids,_ = kmeans(dat, 10)
   # Assign each sample to a cluster
   idx,_ = vq(dat, centroids)
 
   # Some plotting using numpy's logical indexing
   plot(dat[idx==0,0],dat[idx==0,1], 'ob',
        dat[idx==1,0],dat[idx==1,1], 'or',
-       dat[idx==2,0],dat[idx==2,1], 'og')
+       dat[idx==2,0],dat[idx==2,1], 'og',
+       dat[idx==3,0],dat[idx==3,1], 'oc',
+       dat[idx==4,0],dat[idx==4,1], 'om',
+       dat[idx==5,0],dat[idx==5,1], 'oy',
+       dat[idx==6,0],dat[idx==6,1], 'ok',
+       dat[idx==7,0],dat[idx==7,1], 'ob',
+       dat[idx==8,0],dat[idx==8,1], 'or',
+       dat[idx==9,0],dat[idx==9,1], 'og')
   plot(centroids[:,0],centroids[:,1], 'sg', markersize=8)
   show()
   # Need to close the figure window after it closes
@@ -64,11 +70,11 @@ def main():
 
   # Make the dendrogram.  Not sure if we need all of the lines below or what,
   # BUT IT WORKS!!!!
-  sample = data_input.select(Orange.data.sample.SubsetIndices2(data_input, 40), 0)
+  sample = data_input.select(Orange.data.sample.SubsetIndices2(data_input, 10), 0)
   root = Orange.clustering.hierarchical.clustering(sample)
   labels = [str(d.get_class()) for d in sample]
   Orange.clustering.hierarchical.dendrogram_draw("hclust-dendrogram.svg", root, labels=labels)
-
+  print "Dendrogram complete"
   
 #def callback(km):
  #   print "Iteration: %d, changes: %d, score: %8.6f" % (km.iteration, km.nchanges, km.score)
